7c51b6c6fd55996e9f14b0d4f89790ff3a5a51f8
mrmegacoffeeman <rudfo05@gmail.com>
2024-10-02 15:40:16 +0900

    1

---
+++ Test01/Test01/main.c
@@ -0,0 +1,18 @@
+/*
+ * Test01.c
+ *
+ * Created: 2024-10-02 오후 12:17:23
+ * Author : user
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
